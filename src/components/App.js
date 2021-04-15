3982204c67bdcbc3f45cd35b18f930e196aedf69
Shaqri <peter200970@hotmail.com>
2021-04-15 12:48:58 -0500

    Setup github workflows and fix eslint errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-function App () {
-    return(<div>
-        Hello there
-    </div>);
+function App() {
+  return (
+    <div>
+      Hello there
+    </div>
+  );
 }
 
 export default App;
