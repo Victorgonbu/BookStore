47c9195463c176d1b2c3714d3682aa44a7624258
Shaqri <peter200970@hotmail.com>
2021-04-15 12:55:28 -0500

    Add app style css module

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import * as styles from '../style/app.module.css';
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       Hello there
     </div>
   );
