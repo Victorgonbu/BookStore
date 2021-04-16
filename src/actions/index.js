9112a4916396c58588a64b39c15e831ef8692857
Shaqri <peter200970@hotmail.com>
2021-04-16 12:19:52 -0500

    Modify current actions arguments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
-function createBook () {
-    return { type: CREATE_BOOK };
+function createBook (book) {
+    return { type: CREATE_BOOK, payload: book };
 }
 
-function RemoveBook() {
-    return { type: REMOVE_BOOK };
+function RemoveBook(book) {
+    return { type: REMOVE_BOOK, payload: book };
 }
 
 export { createBook, RemoveBook};
