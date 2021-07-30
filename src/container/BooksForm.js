4a6367a6d5d6c08cad55eca32ba82be18425495e
VMB <peter200970@hotmail.com>
2021-07-30 00:11:29 -0500

    Add css style to BookForm componenent

--- src/container/BooksForm.js
+++ src/container/BooksForm.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import {
+  Select, MenuItem, InputLabel, FormControl, TextField,
+} from '@material-ui/core';
 import { createBook } from '../actions/index';
-import { bookForm } from '../style/app.module.css';
+import {
+  bookForm, bookFormWrapper, addBookTitle, categoriesSelect,
+  titleInput, addBookButton,
+} from '../style/app.module.css';
 
 function BooksForm(props) {
   const { createBook } = props;
   const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   const generateProgress = () => Math.floor(Math.random() * 100);
-  const [bookFields, setBookFields] = useState({ title: '', category: 'Action', progress: generateProgress() });
+  const [bookFields, setBookFields] = useState({ title: '', category: '', progress: generateProgress() });
 
-  const opt = (CATEGORIES) => CATEGORIES.map((ct) => (<option key={ct} value={ct}>{ct}</option>));
+  const opt = (CATEGORIES) => CATEGORIES.map((ct) => (
+    <MenuItem
+      key={ct}
+      value={ct}
+    >
+      {ct}
+    </MenuItem>
+  ));
 
   const handleChange = (e) => {
     setBookFields((state) => ({ ...state, [e.target.name]: e.target.value }));
@@ -19,31 +32,33 @@ function BooksForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     createBook(bookFields);
-    setBookFields({ title: '', category: 'Action', progress: generateProgress() });
+    setBookFields({ title: '', category: 'Category', progress: generateProgress() });
   };
 
   return (
-    <form className={bookForm}>
-      <div className="title">
-        <label htmlFor="title">
-          Title
-          <input value={bookFields.title} onChange={handleChange} name="title" id="title" type="text" />
-        </label>
-
-      </div>
-      <div className="category">
-        <label htmlFor="category">
-          Category
-          <select value={bookFields.category} onChange={handleChange} name="category" id="category">
+    <div className={bookFormWrapper}>
+      <h3 className={addBookTitle}>ADD NEW BOOK</h3>
+      <form className={bookForm}>
+        <TextField
+          value={bookFields.title}
+          onChange={handleChange}
+          name="title"
+          id="standard-basic"
+          label="Title"
+          className={titleInput}
+        />
+        <FormControl className={categoriesSelect}>
+          <InputLabel id="category" htmlFor="category">Category</InputLabel>
+          <Select labelId="category" value={bookFields.category} onChange={handleChange} name="category" id="category">
             {opt(CATEGORIES)}
-          </select>
-        </label>
+          </Select>
+        </FormControl>
 
-      </div>
-      <div>
-        <input onClick={handleSubmit} type="submit" value="Submit" />
-      </div>
-    </form>
+        <input className={addBookButton} onClick={handleSubmit} type="submit" value="ADD BOOK" />
+
+      </form>
+    </div>
+
   );
 }
 
